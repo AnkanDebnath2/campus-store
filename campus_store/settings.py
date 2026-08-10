1d6b1c4e0e4738f8c062da8e76a685044a41dadd
AnkanDebnath2 <ankandebnath700@gmail.com>
2026-08-10 09:16:04 +0600

    Explicitly set STATICFILES_STORAGE for cloudinary-storage compatibility

--- campus_store/settings.py
+++ campus_store/settings.py
@@ -263,7 +263,7 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
