cd11a31d337c781575d51e3a110a2cc4a5fe3196
AnkanDebnath2 <ankandebnath700@gmail.com>
2026-08-11 10:08:38 +0600

    update build.sh with collectstatic clear

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py collectstatic --clear --noinput
 python manage.py migrate --database=default --noinput
 python manage.py migrate --database=auth_db --noinput
 python manage.py migrate --database=orders_db --noinput
